432d3d47ce13cf0645f69fc61b1c4c6eb6429b43
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-03-13 16:18:01 +0300

    Add sidebar trigger component.

--- components/organisms/shared/AppHeader.tsx
+++ components/organisms/shared/AppHeader.tsx
@@ -2,12 +2,13 @@
 
 import { HeaderBreadcrumb } from "@/components/organisms/shared/HeaderBreadcrumb";
 import { Separator } from "@/components/ui/separator";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 import { PanelLeft } from "lucide-react";
 
 function AppHeader(){
     return(
         <div className="flex flex-row items-center py-2 pl-2 gap-2">
-            <PanelLeft className="text-gray-200"/>
+            <SidebarTrigger />
             <span> | </span>
             <HeaderBreadcrumb />
         </div>
