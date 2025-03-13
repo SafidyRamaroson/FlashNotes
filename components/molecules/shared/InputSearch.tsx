aeb505dc4d7518f75f1c8661d95a4079fc6fa0d1
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-03-13 16:15:22 +0300

    Minimize style .

--- components/molecules/shared/InputSearch.tsx
+++ components/molecules/shared/InputSearch.tsx
@@ -5,22 +5,19 @@ import React from "react";
 
 type InputSearchProps = {
   placeholder: string;
-  value: string | undefined;
+  value: string | null;
   setValue: (value: string) => void;
 };
 
 function InputSearch({ value, placeholder, setValue }: InputSearchProps) {
   return (
-    <div className="relative flex items-center w-full sm:w-56">
-      <span className="absolute right-2 top-1/2 transform -translate-y-1/2 bg-violet-800 text-white rounded-full py-1 px-2 text-base mt-[2px] font-bold">
-        Ctrl K
-      </span>
+    <div className="relative flex items-center w-full flex-1">
       <Input
         type="text"
-        value={value}
+        value={value || undefined}
         onChange={(e) => setValue(e.target.value)}
         placeholder={placeholder}
-        className="rounded-full w-full h-[40px] pl-4 pr-10 text-base bg-white-50 bg-violet-50"
+        className="rounded-md w-full h-[40px] pl-4 pr-10 text-base bg-white-50"
       />
     </div>
   );
