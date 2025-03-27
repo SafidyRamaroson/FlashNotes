0a9e7013ab191e1dc4fb63f09c4084c8a17dff75
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-03-27 12:39:46 +0300

    Linked SYMBOLS.ITagsRepository abstract to Tags Repository Implementation.

--- core/di/modules/tag.module.ts
+++ core/di/modules/tag.module.ts
@@ -3,11 +3,15 @@ import { SYMBOLS } from "../di.types";
 
 import { ITagsRepository } from "@/core/application/repositories/tags.repo.interface";
 import { TagsUseCase } from "@/core/application/use-cases/tags-use-case";
+import { TagsRepositoryImpl } from "@/core/infrastructure/repositories/tag.repository";
 
 export function createTagsModule(){
     const tagsModule =  createModule();
 
-
+    tagsModule
+        .bind(SYMBOLS.ITagsRepository)
+        .toClass(TagsRepositoryImpl);
+    
     tagsModule
         .bind(SYMBOLS.TagsUseCase)
         .toClass(TagsUseCase, [
