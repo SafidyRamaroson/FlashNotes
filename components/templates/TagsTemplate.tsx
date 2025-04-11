26492e5db3f7a487ea2d5d2b32ec3d2cb47772c7
SafidyRamaroson <safidyramaroson.patrick@gmail.com>
2025-04-11 10:32:53 +0300

    Fetch and display user-specific tags in TagsTemplate component

--- components/templates/TagsTemplate.tsx
+++ components/templates/TagsTemplate.tsx
@@ -6,16 +6,32 @@ import { Plus } from "lucide-react"
 import { CardTag } from "@/components/molecules/cards/CardTag"
 import { useDialogs } from "@/store/useDialogs"
 import { CreateNewTagDialog } from "../organisms/dialogs/CreateNewTag.dialog"
+import { useGet } from "@/hooks/useGet"
+import { useEffect, useState } from "react"
 
 function TagsTemplate() {
     const tags = Array(8).fill(0);
     const { openDialog } = useDialogs();
+    const [tagsUser, setTagsUser] = useState<any>(); 
+    const { data: responseData, isFetched } = useGet({
+        endpoint: "/api/notes/tags",
+        queryKey: "tagsUser",
+        where: {
+          userId: "user1"
+        }
+      });
+
+    useEffect(() => {
+        if(responseData){
+            setTagsUser(responseData.data)
+        }
+    }, [isFetched])
     return (
         <>
             <div className="flex flex-row justify-between items-start">
                 <LayoutSectionHeader
                     title="Gestion des tags"
-                    subtitle="Vous avez 4 tags"
+                    subtitle={`Vous avez ${tagsUser?.length ?? 0}  tags`}
                 />
                 <Button
                     label="Ajouter"
@@ -27,7 +43,7 @@ function TagsTemplate() {
             <CreateNewTagDialog />
             {/* List of tags */}
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
-                {tags.map((tag, idx) => <CardTag key={idx}/>)}
+                { tagsUser?.map((tag) => <CardTag key={tag.id} id={tag.id} name={tag.name} notesCountOfRelatedTag={tag.notes.count}/>) }
             </div>
         </>
     )
